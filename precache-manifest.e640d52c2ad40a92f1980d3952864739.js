6df8b4c4789b3fa764894c718215809dde6c96a2
itemka <itemka2503@gmail.com>
2019-10-19 21:52:01 +0300

    Updates

---
+++ precache-manifest.e640d52c2ad40a92f1980d3952864739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b5486eb2d37b8dd85267666d570243",
+    "url": "/Counter-Redux-/index.html"
+  },
+  {
+    "revision": "0e573f2748f96468a9fc",
+    "url": "/Counter-Redux-/static/css/main.43fb9de8.chunk.css"
+  },
+  {
+    "revision": "99eecb7a4be89053b805",
+    "url": "/Counter-Redux-/static/js/2.6aa7f6cf.chunk.js"
+  },
+  {
+    "revision": "0e573f2748f96468a9fc",
+    "url": "/Counter-Redux-/static/js/main.1634b416.chunk.js"
+  },
+  {
+    "revision": "87773434d66532511c14",
+    "url": "/Counter-Redux-/static/js/runtime~main.f453fd09.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Counter-Redux-/precache-manifest.e640d52c2ad40a92f1980d3952864739.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Counter-Redux-/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
